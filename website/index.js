4793ce4b4ee186f98edd4d9c91de5bad12642fbb
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-06-26 20:31:49 -0500

    Added css styling

--- website/index.js
+++ website/index.js
@@ -1,8 +1,20 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import axios from "axios";
 
+// these two lines let us use __dirname when in an ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+// 1) Tell Express where your templates live
+app.set("views", path.join(__dirname, "views"));
+// 2) Tell Express to use EJS for all ".ejs" files
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +28,7 @@ function isValidUrl(str) {
 }
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { result: null });
+    res.render("index", { result: null });
 });
 
 app.post("/geturl", async (req, res) => {
